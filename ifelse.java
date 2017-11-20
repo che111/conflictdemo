d177ca36d7b886e6d3a92e793dfa02d27831f0ce
chetan <chetan290@gmail.com>
2017-11-20 11:55:00 +0530

    commit by chetan

---
+++ ifelse.java
@@ -0,0 +1,39 @@
+package java_project;
+
+public class ifelse {
+
+	public static void main(String[] args) 
+	{
+	 /* int num=(int)(Math.random()*20);
+	  System.out.println(num);
+	  if (num>=10) //Example of 
+	  {
+		System.out.println("Number is Greater than 10");
+
+	  }
+	   else
+		if (num>=10 && num>=5)
+		{
+			System.out.println(num+" Number is between 10 and 5");
+
+		}
+		else
+		{
+			System.out.println("Number is less than 5");
+
+		}*/
+		int i=1;
+		
+	 /*  while (i<=10) //Example of while loop
+	  {
+		  System.out.println(i);
+		  i++;
+		  
+	  }*/
+		for(i=1;i<=10;i++)//Example of for loop 
+		{
+			System.out.println(i);
+		
+		}
+	}
+}
